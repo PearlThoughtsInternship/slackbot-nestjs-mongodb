9f66bf43b9c403b93daba28fe1f02b17eda2ae4b
Devi <devipushpa12@gmail.com>
2022-04-13 03:01:07 +0530

    fixed new sender iciotp

---
+++ src/providers/blocks/iciotp/viewIciotpFundTransfer.ts
@@ -0,0 +1,39 @@
+export function viewIciotpFundTransfer({OTP,amount,account,payee}: any): any {
+    const blocks = [
+        {
+            "type": "header",
+            "text": {
+                "type": "plain_text",
+                "text": "Fund transfer / (NEFT/IMPS)"
+            }
+        },
+        {
+            "type": "section",
+            "fields": [
+                {
+                    "type": "mrkdwn",
+                    "text": (account==undefined) ? " " : "*ICICI Bank :*\n " +  account
+                },
+                {
+                    "type": "mrkdwn",
+                    "text": (OTP==undefined) ? " " :"*OTP:*\n" + OTP
+                }
+            ]
+        },
+        {
+            "type": "section",
+            "fields": [
+                {
+                    "type": "mrkdwn",
+                    "text": (amount === undefined) ? " " : "*Amount:*\n₹" + amount
+                },
+                {
+                    "type": "mrkdwn",
+                    "text": (payee === undefined) ? " " : "*Payee:*\n" + payee
+                }
+            ]
+        }
+    ];
+
+    return blocks;
+}
