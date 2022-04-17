654eb3e1336ec8d3713580ec4087a7d49d3e8650
Visagalakshmi <vvisagalakshmi@gmail.com>
2022-04-17 17:45:01 +0530

    added-migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,16 +26,7 @@ import { LoggerModule } from 'nestjs-rollbar';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-      entities: [WorkspaceModel, MessageModel, ChannelModel],
-      synchronize: false,
-    }),
+    TypeOrmModule.forRoot(),
     LoggerModule.forRoot({
       accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
       captureUncaught: true,
