6f4a31a661afb0d3e2a19b06534dcfdf3305ae11
Visagalakshmi <vvisagalakshmi@gmail.com>
2022-06-22 10:03:59 +0530

    MR-65 Added exception endpoint to verify rollbar logging

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { RollbarHandler } from 'nestjs-rollbar';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  @RollbarHandler()
+  getException():string {
+    throw new Error("Intentional Exception to verify Rollbar functionality");  
   }
 }
